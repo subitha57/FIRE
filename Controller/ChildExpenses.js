f815e35225ab3fc2f18af088b200b0f2b91f6683
Elon-Rama <ramadevi@elonnativesystem.com>
2024-10-17 10:21:45 +0530

    update-childExpenses

--- Controller/ChildExpenses.js
+++ Controller/ChildExpenses.js
@@ -1,194 +1,71 @@
-// const ChildExpenses = require("../Model/ChildExpensesModel");
-// const User = require("../Model/emailModel");
-// const ExpensesMaster = require("../Model/expensesModel");
-
-// exports.upsert = async (req, res) => {
-//   //#swagger.tags = ['Child-Expenses']
-//   const { id, userId, expensesId, category, title } = req.body;
-
-//   try {
-//     const user = await User.findById(userId);
-//     if (!user) {
-//       return res.status(404).json({ message: "User not found" });
-//     }
-
-//     const expensesMaster = await ExpensesMaster.findById(expensesId);
-//     if (!expensesMaster) {
-//       return res.status(404).json({ message: "Expenses Master not found" });
-//     }
-
-//     const existingExpense = await ChildExpenses.findOne({ userId, expensesId });
-
-//     if (existingExpense) {
-//       const titleExists = existingExpense.category.some((cat) => cat === title);
-
-//       if (titleExists && (!id || existingExpense._id.toString() !== id)) {
-//         return res.status(409).json({
-//           message: `The title '${title}' already exists in the category list for this user.`,
-//         });
-//       }
-//     }
-
-//     if (id) {
-//       const updatedExpense = await ChildExpenses.findByIdAndUpdate(
-//         id,
-//         { userId, expensesId, category, title },
-//         { new: true, upsert: true }
-//       );
-
-//       return res.status(200).json({
-//         message: "ChildExpense Updated Successfully",
-//         data: updatedExpense,
-//       });
-//     } else {
-//       const newChildExpense = new ChildExpenses({
-//         userId,
-//         expensesId,
-//         category,
-//         title,
-//       });
-
-//       await newChildExpense.save();
-
-//       return res.status(201).json({
-//         message: "ChildExpense Added Successfully",
-//         data: newChildExpense,
-//       });
-//     }
-//   } catch (error) {
-//     console.error(error);
-//     return res.status(500).json({
-//       message: "Server error",
-//       error: error.message,
-//     });
-//   }
-// };
-
-// exports.getAll = async (req, res) => {
-//   //#swagger.tags = ['Child-Expenses']
-//   const { userId } = req.params;
-
-//   try {
-//     const childExpenses = await ChildExpenses.find({ userId })
-//       .populate("userId")
-//       .populate("expensesId");
-
-//     if (!childExpenses.length) {
-//       return res.status(404).json({
-//         message: "No expenses found for this user",
-//       });
-//     }
-
-//     return res.status(200).json({
-//       message: "Expenses retrieved successfully",
-//       data: childExpenses,
-//     });
-//   } catch (error) {
-//     console.error(error);
-//     return res.status(500).json({
-//       message: "Server error",
-//       error: error.message,
-//     });
-//   }
-// };
-
 const ChildExpenses = require("../Model/ChildExpensesModel");
-const User = require("../Model/emailModel");
 const ExpensesMaster = require("../Model/expensesModel");
 
 exports.upsert = async (req, res) => {
   //#swagger.tags = ['Child-Expenses']
-  const { id,  expensesId, category} = req.body;
 
   try {
-    ``
+    const { id, expensesId, category } = req.body;
 
-    const expensesMaster = await ExpensesMaster.findById(expensesId);
-    if (!expensesMaster) {
-      return res.status(404).json({ message: "Expenses Master not found" });
-    }
-
-    const existingExpense = await ChildExpenses.findOne({ expensesId });
-
-    if (existingExpense) {
-      const titleExists = existingExpense.category.some((cat) => cat === title);
-
-      if (titleExists && (!id || existingExpense._id.toString() !== id)) {
-        return res.status(409).json({
-          message: `The title '${title}' already exists in the category list for this user.`,
-        });
-      }
+    if (!expensesId || !category) {
+      return res
+        .status(400)
+        .json({ message: "ExpensesId and category are required" });
     }
 
     if (id) {
       const updatedExpense = await ChildExpenses.findByIdAndUpdate(
         id,
-        {  expensesId, category },
-        { new: true, upsert: true }
-      );
+        { expensesId, category },
+        { new: true }
+      ).populate("expensesId", "title");
 
-      return res.status(200).json({
-        message: "ChildExpense Updated Successfully",
-        data: updatedExpense,
-      });
+      if (updatedExpense) {
+        return res.status(200).json({
+          message: "ChildExpenses updated successfully",
+          data: updatedExpense,
+        });
+      } else {
+        return res.status(404).json({ message: "ChildExpenses not found" });
+      }
     } else {
       const newChildExpense = new ChildExpenses({
-        
         expensesId,
         category,
-        
       });
 
       await newChildExpense.save();
 
+      const populatedExpense = await newChildExpense
+        .populate("expensesId", "title")
+        .execPopulate();
+
       return res.status(201).json({
-        message: "ChildExpense Added Successfully",
-        data: newChildExpense,
+        message: "ChildExpenses created successfully",
+        data: populatedExpense,
       });
     }
   } catch (error) {
-    console.error(error);
-    return res.status(500).json({
-      message: "Server error",
-      error: error.message,
-    });
+    console.error("Error in upserting child expenses:", error);
+    return res.status(500).json({ message: "Internal server error" });
   }
 };
 
 exports.getAll = async (req, res) => {
-    //#swagger.tags = ['Child-Expenses']
-    try {
-      const expenses = await ChildExpenses.find();
-      res.status(200).json({
-        statusCode: '0',
-        message: "Expenses data retrived Successfully",
-        data: expenses,
-      });
-    } catch (error) {
-      res.status(500).json({
-        statusCode: '1',
-        message: "Failed to retrived expenses data",
-      });
-    }
-  };
+  //#swagger.tags = ['Child-Expenses']
 
-  exports.deleteById = async (req, res) => {
-    //#swagger.tags = ['Child-Expenses']
-    try {
-      const expenses = await ChildExpenses.findOne({ _id: req.params.expenses_id });
-      if (expenses) {
-        await ChildExpenses.deleteOne({ _id: req.params.expenses_id });
-        res.status(200).json({
-          message: "expenses data deleted successfully",
-        });
-      } else {
-        res.status(404).json({
-          message: "No expenses data found",
-        });
-      }
-    } catch (error) {
-      res.status(500).json({
-        message: "Failed to delete a expenses ",
-      });
-    }
-  };
+  try {
+    const childExpenses = await ChildExpenses.find().populate(
+      "expensesId",
+      "title"
+    );
+
+    return res.status(200).json({
+      message: "ChildExpenses fetched successfully",
+      data: childExpenses,
+    });
+  } catch (error) {
+    console.error("Error in fetching child expenses:", error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
--- Controller/expensesmasterController.js
+++ Controller/expensesmasterController.js
@@ -1,45 +1,60 @@
-
 const ExpensesMaster = require("../Model/expensesModel");
-const User = require("../Model/emailModel");
 
 exports.upsertExpense = async (req, res) => {
   //#swagger.tags = ['Master-Expenses']
-    const { id, title } = req.body;
-    try {
-      
-      if (id) {
-        const updatedExpense = await ExpensesMaster.findByIdAndUpdate(id, { title}, {new: true, upsert: true })
-        res.status(200).json({ statusCode: '0', data: updatedExpense, message: 'MasterExpenses Updated Successfully' })
-      } else {
-        const newExpensesMaster = await new ExpensesMaster({ title }).save();
-        res.status(200).json({ statusCode: '0', data: newExpensesMaster, message: 'MasterExpenses Added Successfully' })
-      }
-    } catch (error) {
-      res.status(500).json({ statusCode: '1', message: error.message })
+  const { id, title } = req.body;
+  try {
+    if (id) {
+      const updatedExpense = await ExpensesMaster.findByIdAndUpdate(
+        id,
+        { title },
+        { new: true, upsert: true }
+      );
+      res
+        .status(200)
+        .json({
+          statusCode: "0",
+          data: updatedExpense,
+          message: "MasterExpenses Updated Successfully",
+        });
+    } else {
+      const newExpensesMaster = await new ExpensesMaster({ title }).save();
+      res
+        .status(200)
+        .json({
+          statusCode: "0",
+          data: newExpensesMaster,
+          message: "MasterExpenses Added Successfully",
+        });
     }
+  } catch (error) {
+    res.status(500).json({ statusCode: "1", message: error.message });
   }
-  
+};
+
 exports.getAll = async (req, res) => {
   //#swagger.tags = ['Master-Expenses']
   try {
     const expenses = await ExpensesMaster.find();
     res.status(200).json({
-      statusCode: '0',
+      statusCode: "0",
       message: "Expenses data retrived Successfully",
       data: expenses,
     });
   } catch (error) {
     res.status(500).json({
-      statusCode: '1',
+      statusCode: "1",
       message: "Failed to retrived expenses data",
     });
   }
 };
 
 exports.getById = async (req, res) => {
- //#swagger.tags = ['Master-Expenses']
+  //#swagger.tags = ['Master-Expenses']
   try {
-    const expenses = await ExpensesMaster.findOne({ _id: req.params.expenses_id });
+    const expenses = await ExpensesMaster.findOne({
+      _id: req.params.expenses_id,
+    });
     if (expenses) {
       res.status(200).json({
         statusCode: "0",
@@ -61,7 +76,9 @@ exports.getById = async (req, res) => {
 exports.deleteById = async (req, res) => {
   //#swagger.tags = ['Master-Expenses']
   try {
-    const expenses = await ExpensesMaster.findOne({ _id: req.params.expenses_id });
+    const expenses = await ExpensesMaster.findOne({
+      _id: req.params.expenses_id,
+    });
     if (expenses) {
       await ExpensesMaster.deleteOne({ _id: req.params.expenses_id });
       res.status(200).json({
@@ -78,5 +95,3 @@ exports.deleteById = async (req, res) => {
     });
   }
 };
-
-
